7b637fcb0f2352d707044d68b9691bd14662b165
MikaLeonhart <00litis00@gmail.com>
2023-08-19 22:29:08 +0300

    V0.002

--- src/App.js
+++ src/App.js
@@ -5,23 +5,19 @@ import Generate from './templates/Windows/Generate';
 
 export default function App() {
 
-    
-
    const [site, setSite] = useState({
       name : '',
       descr : '',
       headerFixed : '',
-      headerColor : ''
-
+      headerColor : '',
+      menu: [],
     //   logo : new FileReader(),
    }); 
-
 
    function edit(prop, event) {
     //   const copy = Object.assign({}, site);
       const copy = { ...site };
         
-
       if (prop === 'headerFixed') {
         copy[prop] = event.target.checked;
       } else {
@@ -30,12 +26,35 @@ export default function App() {
       setSite(copy);
    }
 
-
     const [showComponent, setShowComponent] = useState(true);
 
     const handleGenerateClick = () => {
         setShowComponent(false);
     };
+
+
+    ///////////////////////////////////////////////////////
+    const [tabs, setTabs] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const [isEditable, setIsEditable] = useState(true);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleAddTab = () => {
+        if (tabs.length < 7) {
+            setTabs([...tabs, inputValue.trim()]);
+            setInputValue('');
+        }
+    };
+
+    const handleSaveTabs = () => {
+        setIsEditable(false);
+        site.menu = tabs.map((tab) => ({ name: tab }));
+        console.log(site.menu); // Выводим новый массив объектов в консоль
+    }; 
+    //////////////////////////////////////////////////////////
 
     return (
         <div className='container'>
@@ -49,6 +68,22 @@ export default function App() {
                         <h3>Description</h3>
                         <input className='w-50' type="text" value={site.descr} onChange={event => edit('descr', event)} />
                     </label>
+
+
+                    <label className='mt-2'>
+                        <h3>Menu</h3>
+                        <input type="text" value={inputValue} onChange={handleInputChange} disabled={!isEditable} />
+                        <button onClick={handleAddTab} disabled={!isEditable}>Add</button>
+                        <button onClick={handleSaveTabs} disabled={!isEditable}>Save</button>
+                        <ul>
+                            {tabs.map((tab, index) => (
+                                <li key={index}>{tab}</li>
+                                
+                            ))}
+                        </ul> 
+                    </label>
+
+
                     {/* <label className='mt-2'>
                         <h3>Header logo</h3>
                         <input className=''  name="logo" type="file" onChange={event => edit('logo', event)} />
--- src/templates/Header/header0.js
+++ src/templates/Header/header0.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Header0 = ({site}) => {
     let headerColor = "background-color: " + site.headerColor
     let fixed = "navbar navbar-expand-md border-bottom"
+
     if(site.headerFixed) {
         fixed += " sticky-top"
     } 
+
     return (
       <nav className={fixed} data-bs-theme="dark" style={{backgroundColor : site.headerColor}}>
             <div className="container">
@@ -21,21 +23,31 @@ const Header0 = ({site}) => {
                         <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div className="offcanvas-body">
+                        {/*<ul className="navbar-nav flex-grow-1 justify-content-between">*/}
+                        {/*    <li className="nav-item"><a className="nav-link" href="#">*/}
+                        {/*        <svg className="bi" width="24" height="24"></svg>*/}
+                        {/*    </a></li> */}
+                        {/*    <li className="nav-item">{site.name}</li> */}
+
+                            
                         <ul className="navbar-nav flex-grow-1 justify-content-between">
-                            <li className="nav-item"><a className="nav-link" href="#">
-                                <svg className="bi" width="24" height="24"></svg>
-                            </a></li> 
-                            <li className="nav-item">{site.name}</li> 
-                            <li className="nav-item"><a className="nav-link" href="#">Tour</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">Product</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">Features</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">Enterprise</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">Support</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">Pricing</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">
-                                <svg className="bi" width="24" height="24"></svg>
-                            </a></li>
-                        </ul>
+                            <li className="nav-item"><h3>{site.name}</h3></li> 
+
+                                {site.menu.map((item, index) => (
+                                    <li className="nav-item"><a className="nav-link" href="#" key={index}>{item.name} </a></li>
+                                ))}
+                          </ul>
+
+                            {/*<li className="nav-item"><a className="nav-link" href="#">{site.menu[0].name}</a></li>*/}
+                            {/*<li className="nav-item"><a className="nav-link" href="#">{site.menu[1].name}</a></li>*/}
+                            {/*<li className="nav-item"><a className="nav-link" href="#">{site.menu[2].name}</a></li>*/}
+                            {/*<li className="nav-item"><a className="nav-link" href="#">{site.menu[3].name}</a></li>*/}
+                            {/*<li className="nav-item"><a className="nav-link" href="#">{site.menu[4].name}</a></li>*/}
+                            {/*<li className="nav-item"><a className="nav-link" href="#">{site.menu[5].name}</a></li>*/}
+                            {/*<li className="nav-item"><a className="nav-link" href="#">*/}
+                                {/*<svg className="bi" width="24" height="24"></svg>*/}
+                            {/*</a></li>*/}
+                       {/* </ul>*/}
                     </div>
                 </div>
             </div>
